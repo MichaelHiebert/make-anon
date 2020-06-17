df3419a68da4c6051723b3f87e5ece21d8cb90a9
Michael Hiebert <mhiebert@mit.edu>
2020-06-17 11:45:22 -0400

    Cleaning up repo, renaming to redactme, updating README

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
-# make-anon
-open source image anonymizer
+# RedactMe
+An open source image anonymizer.
+
+<img src="assets/protest.jpg" width="300"/> <img src="assets/redacted.jpg" width="300"/> 
+
+## Table of Contents
+
+- [Quickstart](#quickstart)
+    - [Environment](#environment)
+- [Gallery](#gallery)
+
+# Quickstart
 
 ## Environment
 ```
 conda env create -f environment.yml
 conda activate make-anon
 ```
+
+# Gallery
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from make_anon import image,faces
+from redactme import image,faces
 import cv2
 
 if __name__ == "__main__":
-    frame = image.load_image_to_array('protestorig.jpg')
+    frame = image.load_image_to_array('assets/protest.jpg')
     boxes = faces.get_faces_from_frame(frame)
     redacted = image.redact_faces(frame, boxes)
 
-    cv2.imwrite('redacted.jpg', redacted)
+    cv2.imwrite('assets/redacted.jpg', redacted)
 
     # cv2.imshow('1', redacted)
     # cv2.waitKey(0)
